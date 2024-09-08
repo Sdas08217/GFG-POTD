b672cdb13be7131a355350ee12712f223616f67e
Sourav Das <132899011+Sdas08217@users.noreply.github.com>
2024-09-08 11:42:05 +0530

    Minimum Jumps

---
+++ Sep_2024/Sep_08.py
@@ -0,0 +1,21 @@
+# Minimum Jumps
+class Solution:
+	def minJumps(self, arr):
+        curr_reach, max_reach, jump_count = 0, 0, 0
+        for i in range(len(arr) - 1):
+            max_reach = max(max_reach, i + arr[i])
+            if curr_reach == i:
+                if max_reach == i:
+                    return -1
+                jump_count += 1
+                curr_reach = max_reach
+        return jump_count
+
+# Example usage:
+sol = Solution()
+arr = [2, 3, 1, 1, 4]  # Example array
+result = sol.minJumps(arr)
+print(f"Minimum jumps to reach the end: {result}")
+
+#Output
+Minimum jumps to reach the end: 2
